052cd764852d6b4641fe6fb907d9b08d1ec850ac
Lukáš Kaňka <lukaskabc@gmail.com>
2026-02-27 22:39:39 +0100

    update mod id, remove unused code

---
+++ src/main/java/cz/lukaskabc/minecraft/mod/stellarview/mixin_render/StellarViewMixinRenderer.java
@@ -0,0 +1,12 @@
+package cz.lukaskabc.minecraft.mod.stellarview.mixin_render;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(StellarViewMixinRenderer.MODID)
+public class StellarViewMixinRenderer {
+    public static final String MODID = "stellarview_mixin_renderer";
+    public StellarViewMixinRenderer(IEventBus modEventBus, ModContainer modContainer) {
+    }
+}
---
+++ src/main/java/cz/lukaskabc/minecraft/mod/stellarview/mixin_render/StellarViewMixinRendererClient.java
@@ -0,0 +1,11 @@
+package cz.lukaskabc.minecraft.mod.stellarview.mixin_render;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(value = StellarViewMixinRenderer.MODID, dist = Dist.CLIENT)
+public class StellarViewMixinRendererClient {
+    public StellarViewMixinRendererClient(ModContainer container) {
+    }
+}
